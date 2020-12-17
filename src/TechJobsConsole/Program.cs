e055ae5a938942b7afeb4a584569fabaebb24805
Janell Renee Johnson <docj4jesus@gmail.com>
2020-12-16 19:13:12 -0600

    Final minus bonus missions

--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -63,7 +63,8 @@ namespace TechJobsConsole
                     // Fetch results
                     if (columnChoice.Equals("all"))
                     {
-                        Console.WriteLine("Search all fields not yet implemented.");
+                        searchResults = JobData.FindByValue(searchTerm);
+                        PrintJobs(searchResults);
                     }
                     else
                     {
@@ -118,7 +119,28 @@ namespace TechJobsConsole
 
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
         {
-            Console.WriteLine("PrintJobs is not implemented yet");
+           if(someJobs.Count == 0)
+            {
+                Console.WriteLine("Error. No results found.");
+            }
+           
+           else
+            {
+                foreach (Dictionary<string, string> job in someJobs)
+                {
+                    string border = "*****";
+
+                    Console.WriteLine(border + "\n");
+
+                    foreach (KeyValuePair <string, string> item in job)
+                    {
+                        Console.WriteLine($"{item.Key} : {item.Value}");
+                    }
+
+                    Console.WriteLine(border + "\n");
+                }
+            }
+
         }
     }
 }
